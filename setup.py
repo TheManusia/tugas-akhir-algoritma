3e4b6d49c49afef30c80f72018cdfbbefda25783
Ian <minecraftindo0@gmail.com>
2023-04-15 00:38:41 +0700

    Add setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-# TODO: Buat setup database
+from setuptools import setup
+
+setup(
+    name='Kasir CLI',
+    version='0.1',
+    author='TheManusia',
+    description='Kasir CLI dengan MySql',
+    url='https://github.com/TheManusia',
+    python_requires='>=3.7, <4',
+    license='DO WHAT THE FUCK YOU WANT TO PUBLIC',
+    install_requires=[
+        'mysql-connector==2.2.9',
+        'python-dotenv==1.0.0',
+    ],
+    long_description=open('README.md').read(),
+)
